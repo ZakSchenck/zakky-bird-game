2facfad9412d1dbf79b5baf7f9baa32ca1386609
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2023-07-15 22:41:58 -0400

    Sound effects configuring

--- scripts/app.js
+++ scripts/app.js
@@ -107,7 +107,7 @@ var handleGameRestart = function () {
     character.style.transform = "translateY(".concat(translateY, "%) scaleX(-1)");
     window.addEventListener("keydown", keyDownEvent);
     window.addEventListener("keyup", keyUpEvent);
-    backgroundImg.style.backgroundImage = "url('../static/bggif.gif')";
+    backgroundImg.style.backgroundImage = "url('/zakky-bird-game/static/bggif.gif')";
     gameOverScreen.style.display = "none";
     gameScore = 0;
     scoreElement.innerText = gameScore.toString();
